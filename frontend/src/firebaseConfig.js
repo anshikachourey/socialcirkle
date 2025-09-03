fd25651b79c8bfd1f4c06a4ecad5a352cecb6d0e
anshikachourey <anshikachourey10a@gmail.com>
2025-09-02 20:27:47 -0500

    Fix Firebase storage bucket URL

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAAXZw8dSnhAh4OTrC1hpGssvkddn0QDfU",
   authDomain: "socialcirkle-42d8b.firebaseapp.com",
   projectId: "socialcirkle-42d8b",
-  storageBucket: "socialcirkle-42d8b.firebasestorage.app",
+  storageBucket: "socialcirkle-42d8b.appspot.com",
   messagingSenderId: "896069729535",
   appId: "1:896069729535:web:f88b8e547ab7b8dc9a60ca",};
 const app = initializeApp(firebaseConfig);

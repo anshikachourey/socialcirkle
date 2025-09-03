3cbcbee645fef13ca2cf27dba3fcd3ffcaea2e9b
anshikachourey <anshikachourey10a@gmail.com>
2025-09-02 20:20:44 -0500

    Add Firebase config scaffold

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAAXZw8dSnhAh4OTrC1hpGssvkddn0QDfU",
+  authDomain: "socialcirkle-42d8b.firebaseapp.com",
+  projectId: "socialcirkle-42d8b",
+  storageBucket: "socialcirkle-42d8b.firebasestorage.app",
+  messagingSenderId: "896069729535",
+  appId: "1:896069729535:web:f88b8e547ab7b8dc9a60ca",};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
